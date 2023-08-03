a1c21d14bd0f22ad4db69b57ee970049e029c8fb
Himanshusajwan911 <himanshusajwan911@gmail.com>
2023-08-03 23:40:45 +0530

    Added InsufficientBytesException custom exception class

---
+++ src/io/github/himanshusajwan911/steganography/lsb/core/exceptions/InsufficientBytesException.java
@@ -0,0 +1,39 @@
+/**
+ * Author: Himanshu Sajwan
+ * GitHub: https://github.com/HimanshuSajwan911
+ */
+
+package io.github.himanshusajwan911.steganography.lsb.core.exceptions;
+
+/**
+ * Exception thrown to indicate that there are insufficient bytes available for a specific operation.
+ */
+public class InsufficientBytesException extends RuntimeException{
+
+    /**
+     * Constructs an {@code InsufficientBytesException} with no detail message.
+     */
+    public InsufficientBytesException() {
+        super();
+    }
+
+    /**
+     * Constructs an {@code InsufficientBytesException} with the specified detail message.
+     *
+     * @param message the detail message.
+     */
+    public InsufficientBytesException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs an {@code InsufficientBytesException} with the specified detail message and cause.
+     *
+     * @param message the detail message.
+     * @param cause the cause (a {@code Throwable} that is the underlying cause of this exception).
+     */
+    public InsufficientBytesException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+}
